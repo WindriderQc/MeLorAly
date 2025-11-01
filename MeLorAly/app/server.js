cf7dd1e7457f798d3969f3f06af5b054a2ea6a2a
yanik beaulieu <yanikbeaulieu@hotmail.com>
2025-11-01 18:36:32 +0000

    fixing error routes

--- MeLorAly/app/server.js
+++ MeLorAly/app/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const flash = require('connect-flash');
+const expressLayouts = require('express-ejs-layouts');
 const { createClient } = require('@supabase/supabase-js');
 const path = require('path');
 
@@ -18,6 +19,8 @@ const supabase = createClient(
 // Express configuration
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts);
+app.set('layout', 'layout');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
